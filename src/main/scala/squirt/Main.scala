b93bf7f155af413db7a475f132f48724e69c35ee
Toby Thain <toby@telegraphics.com.au>
2012-08-25 02:38:41 -0400

    Set default channel.

--- src/main/scala/squirt/Main.scala
+++ src/main/scala/squirt/Main.scala
@@ -27,6 +27,6 @@ object SquirtBot {
   def main(argv:Array[String]) {
     // All freenode servers listen on ports 6665, 6666, 6667,
     // 6697 (SSL only), 7000 (SSL only), 7070 (SSL only), 8000, 8001 and 8002.
-    (new Bot("irc.freenode.net", 6667, "#ojXsKJOr", randomNick)).run
+    (new Bot("irc.freenode.net", 6667, "#botwar", randomNick)).run
   }
 }
