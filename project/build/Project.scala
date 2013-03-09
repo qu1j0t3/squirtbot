e4630ba4ae967020e8ce16800f319865fdb26a89
Toby Thain <tthain@panvistamedia.com>
2013-03-09 13:16:28 -0500

    Bump httpcomponents version.

--- project/build/Project.scala
+++ project/build/Project.scala
@@ -2,7 +2,7 @@ import sbt._
 
 class Project(info: ProjectInfo) extends DefaultProject(info) {
   override def compileOptions:Seq[CompileOption] = List( /*Verbose,*/ Unchecked, Deprecation)
-  
+
   //val scalatools = "scala-tools" at "http://scala-tools.org/repo-snapshots"
 
   //val scalatest = "org.scalatest" % "scalatest" % "1.0" % "test->default"
@@ -11,7 +11,7 @@ class Project(info: ProjectInfo) extends DefaultProject(info) {
   //val jodatime = "joda-time" % "joda-time" % "1.6"
   override def libraryDependencies = Set(
     // jm.oauth depends on this:
-    "org.apache.httpcomponents" % "httpclient" % "4.2.1",
-    "org.apache.httpcomponents" % "fluent-hc"  % "4.2.1"
+    "org.apache.httpcomponents" % "httpclient" % "4.2.3",
+    "org.apache.httpcomponents" % "fluent-hc"  % "4.2.3"
   ) ++ super.libraryDependencies
 }
